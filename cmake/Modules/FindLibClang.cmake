37658af3bedac1d2731e693aeb4dc0dc3134553b
Stefan Petersen <spe@ciellt.se>
2019-01-10 21:51:14 +0100

    Finds LLVM for compilation by ugly hack.

--- cmake/Modules/FindLibClang.cmake
+++ cmake/Modules/FindLibClang.cmake
@@ -6,12 +6,18 @@ if( WIN32 )
         set( LIBCLANG_LIBNAME "libclang.dll" )
     endif()
 else()
+    set( LIBCLANG_PATHS "/usr/lib/llvm-6.0" )
     set( LIBCLANG_LIBNAME "clang" )
 endif()
 
 find_path( LibClang_INC_DIR clang-c/Index.h PATHS ${LIBCLANG_PATHS} )
 
 find_library( LibClang_LIB_PATH NAMES ${LIBCLANG_LIBNAME} PATHS ${LIBCLANG_PATHS} )
+
+# Should use `llvm-config --includedir`
+set( LibClang_INC_DIR "/usr/lib/llvm-6.0/include" )
+
+set( LibClang_LIB_PATH "/usr/lib/llvm-6.0/lib/libclang.so" )
 
 include( FindPackageHandleStandardArgs )
 find_package_handle_standard_args( LibClang DEFAULT_MSG LibClang_INC_DIR LibClang_LIB_PATH )
